b3bf15433f213e18543667e69367bba0a6cff423
MOHAMED IDAGHDOUR <midaghdour@gmail.com>
2021-11-20 16:19:00 +0100

    crud-b5

---
+++ seeds/index.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp-db', {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for(let i = 0; i < 50; i++){
+        const randomCities = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            location: `${cities[randomCities].city}, ${cities[randomCities].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251/1600x900',
+            description: "This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel. That makes me feel angry! Anyhoo, your net-suits will allow you to experience.",
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
